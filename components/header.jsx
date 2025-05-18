46dc4101171e495b1d7dd99cc74a782b9dec5019
Disha-arch <guptadisha.1009@gmail.com>
2025-05-18 16:25:34 +0530

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -4,6 +4,12 @@ import { CiHeart } from "react-icons/ci";
 import { FaShoppingCart } from "react-icons/fa";
 
 export default function Header() {
+  const [activeDropdown, setActiveDropdown] = useState(null);
+
+  const handleDropdownToggle = (dropdown) => {
+    setActiveDropdown(activeDropdown === dropdown ? null : dropdown);
+  };
+
   return (
     <main className="main">
       <div className="header">
@@ -12,9 +18,120 @@ export default function Header() {
         </div>
         <div className="list-div">
           <ul className="header--ul">
-            <button className="list1">Men</button>
-            <button className="list2">Women</button>
-            <button className="list3">Kids</button>
+            <div className="menu-category">
+              <button
+                className="list1"
+                onClick={() => handleDropdownToggle("men")}
+              >
+                Men
+              </button>
+              {activeDropdown === "men" && (
+                <ul className="dropdown-list">
+                  <li className="dropdown-item">
+                    <a href="#">Shirts</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Shorts & Jeans</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Shoes</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Shirts</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">T-Shirts & Polo</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Originals</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">TrackSuits</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Football Jersey</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Swimwear</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Hoodies & Jackets</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Accessories</a>
+                  </li>
+                </ul>
+              )}
+            </div>
+            <div className="menu-category">
+              <button
+                className="list2"
+                onClick={() => handleDropdownToggle("women")}
+              >
+                Women
+              </button>
+              {activeDropdown === "women" && (
+                <ul className="dropdown-list">
+                  <li className="dropdown-item">
+                    <a href="#">T-shirts & Tops</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Shorts</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Tights & Leggings</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Sports Bras</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Skirts & Dresses</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Originals</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Joggeres & Track Pants</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Swimwear</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Hoodies & Jackets</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Shoes</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">DripStep Heels</a>
+                  </li>
+                </ul>
+              )}
+            </div>
+            <div className="menu-category">
+              <button
+                className="list3"
+                onClick={() => handleDropdownToggle("kids")}
+              >
+                Kids
+              </button>
+              {activeDropdown === "kids" && (
+                <ul className="dropdown-list">
+                  <li className="dropdown-item">
+                    <a href="#">Toys</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Clothes</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Shoes</a>
+                  </li>
+                  <li className="dropdown-item">
+                    <a href="#">Accessories</a>
+                  </li>
+                </ul>
+              )}
+            </div>
           </ul>
         </div>
         <div className="search--div">
